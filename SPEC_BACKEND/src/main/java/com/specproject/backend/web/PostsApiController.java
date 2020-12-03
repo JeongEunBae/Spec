ed3c832e2e7af3bd5431f6686a572123a091ffb6
배정은 <je_0922@naver.com>
2020-12-04 08:32:20 +0900

    SPEC Project 게시글 CRU API 작업

---
+++ SPEC_BACKEND/src/main/java/com/specproject/backend/web/PostsApiController.java
@@ -0,0 +1,30 @@
+package com.specproject.backend.web;
+
+import com.specproject.backend.domain.posts.PostsService;
+import com.specproject.backend.web.dto.PostsResponseDto;
+import com.specproject.backend.web.dto.PostsSaveRequestDto;
+import com.specproject.backend.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PatchMapping("api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
