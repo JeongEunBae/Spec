6ecc8156acd2b14544ced002454efa8aad04bb8c
배정은 <je_0922@naver.com>
2020-12-05 03:59:20 +0900

    Mustache로 웹연결, (게시글 생성, 조회, 수정, 삭제 구현)

--- SPEC_BACKEND/src/main/java/com/specproject/backend/domain/posts/Posts.java
+++ SPEC_BACKEND/src/main/java/com/specproject/backend/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.specproject.backend.domain.posts;
 
+import com.specproject.backend.domain.BaseEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts{
+public class Posts extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
